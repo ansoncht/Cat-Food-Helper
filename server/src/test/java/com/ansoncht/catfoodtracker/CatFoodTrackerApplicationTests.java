0baad92655eb55784ee71435a97bf4ba7f0588c9
Anson Chung <ansoncht@gmail.com>
2024-10-28 19:14:19 -0700

    [FEATURE] Add simple security to protect endpoints

--- server/src/test/java/com/ansoncht/catfoodtracker/CatFoodTrackerApplicationTests.java
+++ server/src/test/java/com/ansoncht/catfoodtracker/CatFoodTrackerApplicationTests.java
@@ -1,15 +1,10 @@
 package com.ansoncht.catfoodtracker;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootTest
 class CatFoodTrackerApplicationTests {
-
-    @Autowired
-    private MongoTemplate mongoTemplate;
 
     @Test
     void contextLoads() {
